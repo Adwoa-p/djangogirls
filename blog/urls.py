c5ea18820d24b587d7c34c59392774e3f136f1b5
Adwoa-p <pokuaaa158@gmail.com>
2024-10-01 17:59:15 +0000

    added feature to add new posts and extended html file

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('main/', views.post_list, name='post_list'),
-   
-    
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
